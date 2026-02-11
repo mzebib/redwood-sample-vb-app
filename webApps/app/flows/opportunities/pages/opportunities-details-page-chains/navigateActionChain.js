2b9cf677b037e2aeb500dcbd315930457fb5e1f6
Steve Zebib <38408000+mzebib@users.noreply.github.com>
2026-02-11 17:25:06 -0500

    feat(opportunities): add navigate action chain

---
+++ webApps/app/flows/opportunities/pages/opportunities-details-page-chains/navigateActionChain.js
@@ -0,0 +1,27 @@
+define([
+  'vb/action/actionChain',
+  'vb/action/actions',
+  'vb/action/actionUtils',
+], (
+  ActionChain,
+  Actions,
+  ActionUtils
+) => {
+  'use strict';
+
+  class navigateActionChain extends ActionChain {
+
+    /**
+     * @param {Object} context
+     */
+    async run(context) {
+      const { $page, $flow, $application, $constants, $variables } = context;
+
+      const toOptyDetailsEdit = await Actions.navigateToPage(context, {
+        page: 'opportunities-details-edit-page',
+      });
+    }
+  }
+
+  return navigateActionChain;
+});
