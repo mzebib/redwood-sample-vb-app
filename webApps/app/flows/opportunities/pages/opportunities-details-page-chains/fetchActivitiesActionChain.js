a8e2056326c7d5abf7f60457b368476675ebc7bd
Steve Zebib <38408000+mzebib@users.noreply.github.com>
2026-02-24 15:46:36 -0500

    feat: populate activityList in fetchActivitiesActionChain

--- webApps/app/flows/opportunities/pages/opportunities-details-page-chains/fetchActivitiesActionChain.js
+++ webApps/app/flows/opportunities/pages/opportunities-details-page-chains/fetchActivitiesActionChain.js
@@ -20,6 +20,9 @@ define([
       const groupActivitiesByDate = await $functions.groupActivitiesByDate($variables.activityPendingList, $variables.activityCompletedList);
 
       $variables.activitiesGroupList = groupActivitiesByDate;
+
+      const activityList = [...$variables.activityPendingList, ...$variables.activityCompletedList];
+      $variables.activityList = activityList;
     }
   }
 
